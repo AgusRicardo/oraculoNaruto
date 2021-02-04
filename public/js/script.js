328fb4e88822027e74b2d47a436385286a89b9fe
Agustin Ricardo <agustinricardo14@gmail.com>
2021-02-03 21:16:23 -0300

    Falta terminar

---
+++ public/js/script.js
@@ -0,0 +1,329 @@
+
+let baseDeDato = [
+        {
+        "imgCard":"http://oraculo-naruto.web.app/img/cartas/deidara.jpg",
+        "nameCard":"Deidara",
+        "descriptionCard":"Era un ninja perdido de rango S de Iwagakure. Después de desertar de la aldea, se vio obligado a entrar en Akatsuki. Allí, Deidara se asoció con Sasori hasta la muerte de este último, y más tarde con Tobi.",
+        "poderCard": 7
+        }, 
+        {
+        "imgCard":"http://oraculo-naruto.web.app/img/cartas/dog-squad.jpg",
+        "nameCard":"Dog Squad",
+        "descriptionCard":"Son esencialmente perros que tienen sentidos y habilidades intensificados, y como tales, pueden trabajar y ser convocados junto con shinobi",
+        "poderCard": 4
+        }, 
+        {
+        "imgCard":"http://oraculo-naruto.web.app/img/cartas/equipo-cuatro.jpg",
+        "nameCard":"Equipo 7",
+        "descriptionCard":"Equipo conformado por: Naruto Uzumaki, Sakura Haruno, Sasuke Uchiha y Kakashi Hatake como su líder.",
+        "poderCard": 8
+        }, 
+        {
+        "imgCard":"http://oraculo-naruto.web.app/img/cartas/gaara.jpg",
+        "nameCard":"Gaara",
+        "descriptionCard":"Es un shinobi de Sunagakure y el Quinto Kazekage, fue el Comandante General del ejército de la Gran Alianza Shinobi y Comandante de la Cuarta División. Formó parte del ex-equipo Baki.",
+        "poderCard": 8
+        }, 
+        {
+        "imgCard":"http://oraculo-naruto.web.app/img/cartas/guy-sensei.jpg",
+        "nameCard":"Guy Sensei",
+        "descriptionCard":"Es un Shinobi de nivel jōnin de Konohagakure. Es el líder del Equipo Guy, compuesto por Neji Hyūga, Rock Lee y Tenten.",
+        "poderCard": 7
+        }, 
+        {
+        "imgCard":"http://oraculo-naruto.web.app/img/cartas/iruka.jpg",
+        "nameCard":"Iruka Sensei",
+        "descriptionCard":"Es un Chūnin de Konohagakure. Fue instructor y actualmente es el director de la Academia Ninja.",
+        "poderCard": 6
+        }, 
+        {
+        "imgCard":"http://oraculo-naruto.web.app/img/cartas/itachi.png",
+        "nameCard":"Itachi",
+        "descriptionCard":"Era el hermano mayor de Sasuke Uchiha. Fue un prodigioso shinobi de Konohagakure infiltrado en la organización ANBU como un doble espía. Después de masacrar al Clan Uchiha, se unió a Akatsuki.",
+        "poderCard": 10
+        }, 
+        {
+        "imgCard":"http://oraculo-naruto.web.app/img/cartas/jiraiya.jpg",
+        "nameCard":"Jiraiya",
+        "descriptionCard":"Fué uno de los legendarios Sannin junto con Tsunade y Orochimaru, quienes fueron entrenados por el Tercer Hokage.También fue conocido como el Sabio Sapo",
+        "poderCard": 10
+        }, 
+        {
+        "imgCard":"http://oraculo-naruto.web.app/img/cartas/kabuto.jpg",
+        "nameCard":"Kabuto",
+        "descriptionCard":"Ex-médico personal, mano derecha y asistente de Orochimaru.",
+        "poderCard": 8
+        }, 
+        {
+        "imgCard":"http://oraculo-naruto.web.app/img/cartas/kiba.jpg",
+        "nameCard":"Kiba",
+        "descriptionCard":"Es un shinobi de Konohagakure de rango Chūnin perteneciente al Clan Inuzuka. Forma parte del Equipo Kurenai, junto con sus compañeros Hinata Hyūga y Shino Aburame",
+        "poderCard": 7
+        }, 
+        {
+        "imgCard":"http://oraculo-naruto.web.app/img/cartas/killer-bee.jpg",
+        "nameCard":"Killer B",
+        "descriptionCard":"Es un shinobi de Kumogakure y último Jinchūriki de Gyūki. Es el hermano menor adoptivo del Cuarto Raikage e hijo adoptivo del Tercer Raikage",
+        "poderCard": 8
+        }, 
+        {
+        "imgCard":"http://oraculo-naruto.web.app/img/cartas/kisame.jpg",
+        "nameCard":"Kisame",
+        "descriptionCard":"Era un ninja renegado de Kirigakure, ex miembro de los Siete Espadachines Ninja de la Niebla y anteriormente compañero de Itachi Uchiha en Akatsuki.",
+        "poderCard": 9
+        }, 
+        {
+        "imgCard":"http://oraculo-naruto.web.app/img/cartas/minato.jpg",
+        "nameCard":"Minato",
+        "descriptionCard":"Fué el Cuarto Hokage de la Aldea Oculta de la Hoja. Fue discípulo de Jiraiya y maestro de Kakashi Hatake, Obito Uchiha y Rin Nohara. También fue el esposo de Kushina Uzumaki y el padre de Naruto Uzumaki.",
+        "poderCard": 10
+        }, 
+        {
+        "imgCard":"http://oraculo-naruto.web.app/img/cartas/naruto.jpg",
+        "nameCard":"Naruto",
+        "descriptionCard":"Es residente de Konohagakure, hijo del Cuarto Hokage, Minato Namikaze y Kushina Uzumaki. Desde el día de su nacimiento fue el Jinchūriki de la mitad Yang de Kurama.",
+        "poderCard": 10
+        }, 
+        {
+        "imgCard":"http://oraculo-naruto.web.app/img/cartas/naruto-sai.jpg",
+        "nameCard":"Naruto y Sai",
+        "descriptionCard":"Sai es un joven ninja que pertenecía a la Raíz y se especializa en las técnicas que utilizan tinta. Fue un miembro temporal del Equipo Kakashi durante la ausencia de Sasuke Uchiha.",
+        "poderCard": 8
+        }, 
+        {
+        "imgCard":"http://oraculo-naruto.web.app/img/cartas/sakura.jpg",
+        "nameCard":"Sakura",
+        "descriptionCard":"Es una kunoichi de nivel Jōnin, miembro del Equipo Kakashi y una gran amiga de Naruto Uzumaki. Después de su entrenamiento con Tsunade, se convierte en una Ninja Médico",
+        "poderCard": 5
+        }, 
+        {
+        "imgCard":"http://oraculo-naruto.web.app/img/cartas/sasori.png",
+        "nameCard":"Sasori",
+        "descriptionCard":"Era el nieto de Chiyo y miembro de Akatsuki. Tenía como compañero a Deidara",
+        "poderCard": 7
+        }, 
+        {
+        "imgCard":"http://oraculo-naruto.web.app/img/cartas/sasuke.jpg",
+        "nameCard":"Sasuke",
+        "descriptionCard":"Es el hermano de Itachi Uchiha. Sasuke decidió desertar de Konohagakure en busca del poder necesario para vengar a su clan y asesinar a Itachi",
+        "poderCard": 10
+        }, 
+        {
+        "imgCard":"http://oraculo-naruto.web.app/img/cartas/shikamaru.jpg",
+        "nameCard":"Shikamaru",
+        "descriptionCard":"Es un ninja de Konoha que pertenece al Clan Nara. Fue miembro del Equipo Asuma junto con sus compañeros Chōji Akimichi e Ino Yamanaka.",
+        "poderCard": 8
+        }, 
+        {
+        "imgCard":"http://oraculo-naruto.web.app/img/cartas/shino-kiba.jpg",
+        "nameCard":"Shino y Kiba",
+        "descriptionCard":"Shino es un Jōnin de Konohagakure y miembro del Clan Aburame, famoso por su uso de los Kikaichū, formó parte del Equipo Kurenai, junto a sus compañeros Kiba Inuzuka y Hinata Hyūga.",
+        "poderCard": 7
+        }, 
+        {
+        "imgCard":"http://oraculo-naruto.web.app/img/cartas/yamato.jpg",
+        "nameCard":"Yamato",
+        "descriptionCard":"Es un miembro de ANBU, y un ninja de Konohagakure del mismo rango que Kakashi Hatake. Fué el líder temporal del Equipo 7",
+        "poderCard": 7
+        }, 
+        {
+        "imgCard":"http://oraculo-naruto.web.app/img/cartas/zorro.jpg",
+        "nameCard":"Kurama",
+        "descriptionCard":"Es una Bestia con Cola que se encuentra sellada dentro de Naruto Uzumaki, siendo considerada la más poderosa de las nueve bestias creadas por el Sabio de los Seis Caminos.",
+        "poderCard": 10
+        }
+]
+
+
+alert('Hola jony, no está terminado pero bueno. No queria no mandar nada. Un abrazo')
+let page1 = document.getElementById('page1');
+let page2 = document.getElementById('page2');
+let loading = document.getElementById('loading')
+let page4 = document.getElementById('page4');
+
+let inputPlayer1 = document.getElementById('input-player1')
+let inputPlayer2 = document.getElementById('input-player2')
+
+
+
+// Botón de Play a game
+let verificar = document.getElementById('error');
+let btnPlayGame = document.getElementById('btn-play-game').addEventListener('click', function (e) {
+    loading.style.display = 'flex';
+    page1.style.display = 'none';
+    e.preventDefault()
+    setTimeout(function (){
+        inputPlayer1 = document.getElementById('input-player1').value
+        inputPlayer2 = document.getElementById('input-player2').value
+        document.getElementById('input-player1').value = '';
+        document.getElementById('input-player2').value = '';
+        if (inputPlayer1 != '' && inputPlayer2 != '') {
+            loading.style.display = 'none';
+            page1.style.display = 'none';
+            page2.style.display = 'block';
+        }else{
+            loading.style.display = 'none';
+            page1.style.display = 'block';
+            let nodoError = document.createElement('h5')
+            let txtNodoError = document.createTextNode('Ingrese ambos nombres por favor')
+            nodoError.appendChild(txtNodoError)
+            verificar.appendChild(nodoError)
+        }
+    }, 3000)
+});
+
+// Cartas
+let lasSeisCartas = [];
+let cartasRandom = () => {
+    for (let i = 0; i < 6; i++) {
+        lasSeisCartas[i] = baseDeDato[Math.round(Math.random()* 21) + 1];
+    }
+}
+let seisCartasRandom = cartasRandom()
+
+let card1 = document.getElementById('card1');
+let card2 = document.getElementById('card2');
+let card3 = document.getElementById('card3');
+let card4 = document.getElementById('card4');
+let card5 = document.getElementById('card5');
+let card6 = document.getElementById('card6');
+
+
+let mostrarCartas = (data) => {
+    for (let i = 0; i < 6; i++) {
+        card1.innerHTML = `
+        <h3>Jugador 1</h3>
+        <h3>Carta 1/3</h3>
+        <img src="${data[0].imgCard}" alt="">
+        <h1>${data[0].nameCard}</h1>
+        <p>${data[0].descriptionCard}</p>
+        <h5>Valor: ${data[0].poderCard}</h5>
+        `
+        card2.innerHTML = `
+        <h3>Jugador 1</h3>
+        <h3>Carta 2/3</h3>
+        <img src="${data[1].imgCard}" alt="">
+        <h1>${data[1].nameCard}</h1>
+        <p>${data[1].descriptionCard}</p>
+        <h5>Valor: ${data[1].poderCard}</h5>
+        `
+        card3.innerHTML = `
+        <h3>Jugador 1</h3>
+        <h3>Carta 3/3</h3>
+        <img src="${data[2].imgCard}" alt="">
+        <h1>${data[2].nameCard}</h1>
+        <p>${data[2].descriptionCard}</p>
+        <h5>Valor: ${data[2].poderCard}</h5>
+        `
+        card4.innerHTML = `
+        <h3>Jugador 2</h3>
+        <h3>Carta 1/3</h3>
+        <img src="${data[3].imgCard}" alt="">
+        <h1>${data[3].nameCard}</h1>
+        <p>${data[3].descriptionCard}</p>
+        <h5>Valor: ${data[3].poderCard}</h5>
+        `
+        card5.innerHTML = `
+        <h3>Jugador 2</h3>
+        <h3>Carta 2/3</h3>
+        <img src="${data[4].imgCard}" alt="">
+        <h1>${data[4].nameCard}</h1>
+        <p>${data[4].descriptionCard}</p>
+        <h5>Valor: ${data[4].poderCard}</h5>
+        `
+        card6.innerHTML = `
+        <h3>Jugador 2</h3>
+        <h3>Carta 3/3</h3>
+        <img src="${data[5].imgCard}" alt="">
+        <h1>${data[5].nameCard}</h1>
+        <p>${data[5].descriptionCard}</p>
+        <h5>Valor: ${data[5].poderCard}</h5>
+        `
+    }
+}
+let cartasRepartidas = mostrarCartas(lasSeisCartas)
+
+
+
+// Modal
+let modal_BtnResultado = document.getElementById('modal_btn-resultados').addEventListener('click', function () {
+    loading.style.display = 'flex';
+    page2.style.display = 'none'
+    setTimeout(function (){
+        loading.style.display = 'none';
+        page4.style.display = 'block'
+    }, 2000)
+})
+let modal_volverATirar = document.getElementById('modal_btn-volver-a-tirar').addEventListener('click', function () {
+    page1.style.display = 'block'
+    page2.style.display = 'none'
+})
+
+// Pantalla resultados
+let cartasPlayer1 = document.getElementById('cartas-player-1').innerHTML = `
+<img src="${lasSeisCartas[0].imgCard}" alt="">
+<img src="${lasSeisCartas[1].imgCard}" alt="">
+<img src="${lasSeisCartas[2].imgCard}" alt="">
+`
+let cartasPlayer2 = document.getElementById('cartas-player-2').innerHTML = `
+<img src="${lasSeisCartas[3].imgCard}" alt="">
+<img src="${lasSeisCartas[4].imgCard}" alt="">
+<img src="${lasSeisCartas[5].imgCard}" alt="">
+`
+
+let match = document.getElementById('match')
+function matchGenerator() {
+    if (((lasSeisCartas[0].poderCard + lasSeisCartas[1].poderCard + lasSeisCartas[2].poderCard) > 22) && ((lasSeisCartas[3].poderCard + lasSeisCartas[4].poderCard + lasSeisCartas[5].poderCard) > 22)) {
+        match.innerHTML = `
+        <h1>Hubo Match?</h1>
+        <img src="./img/match-si.png" alt="">
+        `
+    }else (
+        match.innerHTML = `
+        <h1>Hubo Match?</h1>
+        <img src="./img/match-no.png" alt="">
+        `
+    )
+}
+matchGenerator(lasSeisCartas)
+
+
+let game1 = []
+let game2 = []
+let game3 = []
+let game4 = []
+
+// Boton para guardar resultados
+let btnGuardar = document.getElementById('result_footer_btn_guardar').addEventListener('click', function () {
+    let listaResultadosAnteriores = document.getElementById('lista-resultados-anteriores')
+    page4.style.display = 'none'
+    page1.style.display = 'block'
+    listaResultadosAnteriores.style.display = `block`
+    if (game1 == '') {
+        listaResultadosAnteriores.innerHTML = `
+        <h1>Juegos anteriores</h1>
+        <li class="list-group-item">Juego 1</li>
+        `
+    }else if (game2 == '') {
+        listaResultadosAnteriores.innerHTML = `
+        <h1>Juegos anteriores</h1>
+        <li class="list-group-item">juego 2</li>
+        `
+    }else if (game3 == '') {
+        listaResultadosAnteriores.innerHTML = `
+        <h1>Juegos anteriores</h1>
+        <li class="list-group-item">juego 3</li>
+        `
+    }else if (game4 == '') {
+        listaResultadosAnteriores.innerHTML = `
+        <h2>Juegos anteriores</h2>
+        <li class="list-group-item">juego 4</li>
+        `
+    }
+})
+
+// Boton para salir de resultados
+let btnSalir = document.getElementById('result_footer_btn_salir').addEventListener('click', function () {
+    page4.style.display= 'none'
+    page1.style.display = 'block'
+})
